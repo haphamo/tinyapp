1ea30e5f3387ca673fff0dd6241429a20413f99b
Ha Pham <hapham0392@gmail.com>
2019-09-30 20:18:22 +0000

    Added header partial

--- express_server.js
+++ express_server.js
@@ -18,6 +18,11 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
@@ -26,14 +31,7 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
- });
- 
- app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
- });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
