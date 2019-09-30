4217bb444de284bc88ff68b731bcc16c36da350e
Ha Pham <hapham0392@gmail.com>
2019-09-30 18:35:12 +0000

    added a route for /urls. the browser will be blank because we haven't created  template yet

--- express_server.js
+++ express_server.js
@@ -13,6 +13,11 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello! Welcome to the HomePage</h1>");
 });
 
+app.get("/urls", (req, res) => {
+  let templateVars = {urls: urlDatabase};
+  res.render("urls_index", templateVars);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
