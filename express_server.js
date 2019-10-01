d7c08b99424d60645e532986086b19d130b30c25
Ha Pham <hapham0392@gmail.com>
2019-10-01 01:02:36 +0000

    created redirects for the short URLS. Code must be clear in redirections.

--- express_server.js
+++ express_server.js
@@ -5,8 +5,13 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
 function generateRandomString() {
-
-}
+  let shortURL = "";
+  let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (let i = 0; i < 6; i++) {
+      shortURL += possible.charAt(Math.floor(Math.random() * possible.length));
+    };
+    return shortURL;
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -29,15 +34,25 @@ app.get("/urls/new", (req, res) => {//Adding a GET route to show the form. The o
   res.render("urls_new");
 });
 
+
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  let rediredShortURL = generateRandomString()
+  urlDatabase[rediredShortURL] = req.body.longURL 
+  console.log('result', '/u/'+ rediredShortURL);
+  res.redirect('/urls/'+ rediredShortURL)
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL
+  longURL = urlDatabase[shortURL];
+  res.redirect(longURL);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
@@ -46,8 +61,6 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
-
-
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
