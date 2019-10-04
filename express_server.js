f6af4784fa61d524bf52dbc43ede4e16ecd6a57e
Ha Pham <hapham0392@gmail.com>
2019-10-04 17:18:05 +0000

    removed console.logs and cleaned up

--- express_server.js
+++ express_server.js
@@ -2,18 +2,15 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bcrypt = require('bcrypt');
+const bodyParser = require("body-parser");
+let cookieSession = require('cookie-session')
 
 app.set("view engine", "ejs");
-const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
-
-
-let cookieSession = require('cookie-session')
 
 app.use(cookieSession({
   name: 'session',
-  keys: ["mituot"],
-
+  keys: ["mituot"]
 }))
 
 function generateRandomString() {
@@ -75,7 +72,7 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello! Welcome to the HomePage</h1>");
 });
 
-app.get("/urls", (req, res) => {//go back to fix this !!!!
+app.get("/urls", (req, res) => {
   user = userDatabase[req.session.user_id]
   if (!user) {//only logged in users can create links otherwise redirect
     res.redirect("/login");
@@ -99,8 +96,6 @@ app.get("/urls/new", (req, res) => {//get route ro create new links
 app.post("/urls", (req, res) => {
   let redirected = generateRandomString()
   urlDatabase[redirected] = { "longURL" : req.body.longURL , userID: req.session.user_id, "shortURL": redirected}
-  console.log('shortURL has been created for ' + req.body.longURL +'\n' + redirected + " for userID: " + req.session.user_id);
-  console.log("urlDatabase:", urlDatabase);
   res.redirect('/urls/'+ redirected);
 });
 
@@ -181,18 +176,12 @@ app.post("/register", (req, res) => {//post method for register, Store the email
 
 app.get("/login", (req, res) => {//get route to log in
   user = userDatabase[req.session.user_id]
-  console.log("userDatabase: ", userDatabase);
-  console.log("urlDatabase", urlDatabase)
-  console.log("user is this one", user)
-  console.log("cookies", req.session.user_id)
   let templateVars = { user };
   res.render("urls_login", templateVars);
 });
 
 app.post("/login", (req, res) => {//post route to log in
   let existingUser = checkEmail(req.body.email);
-  console.log("req.body.password:", req.body.password);
-  
   if (!existingUser) {
     res.status(403).send("email doesn't exist!");
   } else if (!bcrypt.compareSync(req.body.password, userDatabase[existingUser].password )) {
